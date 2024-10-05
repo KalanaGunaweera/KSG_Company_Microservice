0e36f390364e458708341d7608f08e9f6322f8e0
Kalana Gunaweera <kalanagunaweera94@gmail.com>
2024-10-05 09:21:35 +0530

    1st Commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.KSG_Company;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
